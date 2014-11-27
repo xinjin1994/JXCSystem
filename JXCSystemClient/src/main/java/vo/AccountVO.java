d11ac9ecc0200b99e7aea9f170c8999d8480bd5c
lsy <864986781@qq.com>
2014-11-28 02:06:41 +0800

    加了一个accountVO

---
+++ JXCSystemClient/src/main/java/vo/AccountVO.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class AccountVO {
+
+	public String name;
+	public String balance;
+	public AccountVO(String name, String balance) {
+		super();
+		this.name = name;
+		this.balance = balance;
+	}
+	
+}
--- JXCSystemClient/src/main/java/vo/GetVO.java
+++ JXCSystemClient/src/main/java/vo/GetVO.java
@@ -14,4 +14,5 @@ public class GetVO {
 		this.transferList=transferList;
 		this.total=total;
 	}
+
 }
