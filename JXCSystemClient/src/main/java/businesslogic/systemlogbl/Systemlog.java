063f130e85adb141d509df0836acc370c2d685a7
JinXinNJU <xinjinnju@163.com>
2014-12-02 02:10:38 +0800

    关于systemloginfo的一些修改

--- JXCSystemClient/src/main/java/businesslogic/systemlogbl/Systemlog.java
+++ JXCSystemClient/src/main/java/businesslogic/systemlogbl/Systemlog.java
@@ -1,19 +1,23 @@
 package businesslogic.systemlogbl;
 
-
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import po.SystemlogPO;
+import vo.SystemlogVO;
 import businesslogicservice.systemlogblservice.SystemlogblService;
 import data.systemlogdata.SystemlogDataService_Stub;
 import dataservice.systemlogdataservice.SystemlogDataService;
 
-public class Systemlog implements SystemlogblService, businesslogic.commoditybl.SystemlogInfo,
-			businesslogic.accountbl.SystemlogInfo, businesslogic.financialbl.SystemlogInfo,
-			businesslogic.initializationlbl.SystemlogInfo, businesslogic.invoicebl.SystemlogInfo,
-			businesslogic.promotionbl.SystemlogInfo, businesslogic.salesbl.SystemlogInfo,
-			businesslogic.userbl.SystemlogInfo{
-	
+public class Systemlog implements SystemlogblService,
+		businesslogic.commoditybl.SystemlogInfo,
+		businesslogic.accountbl.SystemlogInfo,
+		businesslogic.financialbl.SystemlogInfo,
+		businesslogic.initializationlbl.SystemlogInfo,
+		businesslogic.invoicebl.SystemlogInfo,
+		businesslogic.promotionbl.SystemlogInfo,
+		businesslogic.salesbl.SystemlogInfo, businesslogic.userbl.SystemlogInfo {
+
 	public SystemlogDataService sys = new SystemlogDataService_Stub();
 
 	public SystemlogDataService getSys() {
@@ -24,33 +28,33 @@ public class Systemlog implements SystemlogblService, businesslogic.commoditybl.
 		this.sys = sys;
 	}
 
-	public String show() {
+	public ArrayList<SystemlogVO> show() {
 		// TODO Auto-generated method stub
-		//SystemlogDataService sys = new SystemlogDataService_Stub();
+		// SystemlogDataService sys = new SystemlogDataService_Stub();
 		try {
-			if(sys.get()!=null){
-				return "成功";
+			if (sys.get() != null) {
+				return 0;
 			}
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "失败";
+		return null;
 	}
 
-	public String add(String word) {
+	public int add(String word) {
 		// TODO Auto-generated method stub
-		SystemlogPO system = new SystemlogPO("operation","time");
-		//SystemlogDataService sys = new SystemlogDataService_Stub();
+		SystemlogPO system = new SystemlogPO("operation", "time");
+		// SystemlogDataService sys = new SystemlogDataService_Stub();
 		try {
-			if(sys.add(system)){
-				return "成功";
+			if (sys.add(system)) {
+				return 0;
 			}
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "失败";
+		return 1;// 此数字应该对应某种失败结果
 	}
 
 }
--- JXCSystemClient/src/main/java/businesslogic/userbl/SystemlogInfo.java
+++ JXCSystemClient/src/main/java/businesslogic/userbl/SystemlogInfo.java
@@ -2,6 +2,6 @@ package businesslogic.userbl;
 
 public interface SystemlogInfo {
 
-	public String add(String word);
+	public int add(String word);
 	
 }
