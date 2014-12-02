1029876c2279e6cba163bc9addd21bc679e20e06
JinXinNJU <xinjinnju@163.com>
2014-12-02 15:58:41 +0800

    还没有写具体实现的sales逻辑层

--- JXCSystemClient/src/main/java/businesslogicservice/salesblservice/SalesblService.java
+++ JXCSystemClient/src/main/java/businesslogicservice/salesblservice/SalesblService.java
@@ -9,27 +9,27 @@ import vo.ImportMenuVO;
 public interface SalesblService {
 	
 	//添加客户
-	public String addCustomer(CustomerVO customerVO);
+	public int addCustomer(CustomerVO customerVO);
 	
 	//删除客户
-	public String delCustomer(CustomerVO customerVO);
+	public int delCustomer(CustomerVO customerVO);
 	
 	//修改客户
-	public String updateCustomer(CustomerVO customerVO);
+	public int updateCustomer(CustomerVO customerVO);
 	
 	//查找客户
 	public ArrayList<CustomerVO> searchCustomer(CustomerVO customerVO);
 	
 	//添加进货单
-	public String addImport(ImportMenuVO importMenuVO);
+	public int addImport(ImportMenuVO importMenuVO);
 	
 	//添加进货退货单
-	public String addImport_Return(ImportMenuVO importMenuVO);
+	public int addImport_Return(ImportMenuVO importMenuVO);
 	
 	//添加销售单
-	public String addExport(ExportMenuVO exportMenuVO);
+	public int addExport(ExportMenuVO exportMenuVO);
 	
 	//添加销售退货单
-	public String addExport_Return(ExportMenuVO exportMenuVO);
+	public int addExport_Return(ExportMenuVO exportMenuVO);
 	
 }
