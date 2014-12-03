d2d082837f50bd594b644390c97dfe934b50a1ad
Lf <lfsoftware13@gmail.com>
2014-12-03 21:57:58 +0800

    Account逻辑层部分接口修改
    恩就是这样

--- JXCSystemClient/src/main/java/businesslogicservice/accountblservice/AccountblService.java
+++ JXCSystemClient/src/main/java/businesslogicservice/accountblservice/AccountblService.java
@@ -10,23 +10,26 @@ import vo.PayVO;
 public interface AccountblService {
 	
 	//查看当前账户信息
-	public AccountVO checkAccount();
+	public AccountVO checkAccount_up();
 
 	//添加账户
-	public int addAccount(AccountVO vo);
+	public int addAccount_up(AccountVO vo);
 
 	//删除账户
-	public int delAccount(AccountVO vo);
+	public int delAccount_up(AccountVO vo);
 
 	//修改账户
-	public int updateAccount(AccountVO vo1, AccountVO vo2);
+	public int updateAccount_up(AccountVO vo1, AccountVO vo2);
 
-	//查找账户
-	public ArrayList<AccountVO> searchAccount(String word);
+	//模糊查找账户
+	public ArrayList<AccountVO> searchFuzzyAccount_up(String word);
+	
+	//精确查找账户
+	public ArrayList<AccountVO> searchAccurateAccount_up(AccountVO vo);
 
 	//添加收款单
-	public int addReceipt(GetVO vo);
+	public int addReceipt_up(GetVO vo);
 	//添加付款单
-	public int addPayment(PayVO vo);
+	public int addPayment_up(PayVO vo);
 	
 }
