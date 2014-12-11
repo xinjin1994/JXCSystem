a331ce1fd433dbde5634a02dd599220e93589662
Lf <lfsoftware13@gmail.com>
2014-12-11 19:38:04 +0800

    ConditionVO

--- JXCSystemClient/src/main/java/vo/ConditionVO.java
+++ JXCSystemClient/src/main/java/vo/ConditionVO.java
@@ -2,11 +2,15 @@ package vo;
 
 public class ConditionVO {
 	
-	public ConditionGetVO get;
-	public ConditionPayVO pay;
+	public double sales_income;
+	public double com_income;
+	public double discount;
+	public double sales_outcome;
+	public double com_outcome;
+	public double profit;
 	
 	public double getProfit(){
-		return get.totalPrice-pay.totalPay;
+		return sales_income+com_income-discount-sales_outcome-com_outcome;
 	}
 
 }
