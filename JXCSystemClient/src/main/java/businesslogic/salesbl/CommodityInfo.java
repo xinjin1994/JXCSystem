6094d1ae10a3f05560b0d512cb594ac6b927dedd
JinXinNJU <xinjinnju@163.com>
2014-12-15 21:14:25 +0800

    加了一个接口！
    sales模块中的commodityInfo

--- JXCSystemClient/src/main/java/businesslogic/salesbl/CommodityInfo.java
+++ JXCSystemClient/src/main/java/businesslogic/salesbl/CommodityInfo.java
@@ -1,9 +1,13 @@
 package businesslogic.salesbl;
 
+import java.util.ArrayList;
+
 import po.CommodityPO;
 
 public interface CommodityInfo {
 
 	public CommodityPO findCommodity(String name, String type);
+	
+	public ArrayList<CommodityPO> getAllCommodity();
 	
 }
